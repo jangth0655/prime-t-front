c95238cf82d6472e55c79c70704051d76fc6b623
jangth4185 <jangth4185@gmail.com>
2024-01-29 17:27:37 +0900

    메인 홈 - portfolio section 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Intro from "@/components/mainHome/Intro";
+import Portfolio from "@/components/mainHome/Portfolio";
 import Process from "@/components/mainHome/Process";
 
 export default function HomePage() {
@@ -6,6 +7,7 @@ export default function HomePage() {
     <section className="px-4 bg-slate-S900">
       <Intro />
       <Process />
+      <Portfolio />
     </section>
   );
 }
