613c5628da770381741380e8e357db18c86a36cd
jangth4185 <jangth4185@gmail.com>
2024-01-29 15:34:38 +0900

    svgr 추가, main home - 프로세스 섹션 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/mainHome/Intro";
+import Process from "@/components/mainHome/Process";
 
 export default function HomePage() {
   return (
     <section className="px-4 bg-slate-S900 border-2">
       <Intro />
+      <Process />
     </section>
   );
 }
