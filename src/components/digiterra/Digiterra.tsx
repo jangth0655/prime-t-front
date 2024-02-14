c9df4998df79a92c22851c402d9423ec62b27112
jangth4185 <jangth4185@gmail.com>
2024-02-14 12:09:44 +0900

    공통 Selector 컴포넌트 추가

--- src/components/digiterra/Digiterra.tsx
+++ src/components/digiterra/Digiterra.tsx
@@ -1,22 +1,49 @@
 "use client";
 
 import { useCallback, useState } from "react";
+
 import PageHeader from "../PageHeader";
 import DigiterraList from "./DigiterraList";
-import { CategoryType } from "../common/TabCategory";
+import Selector, { SelectItemType } from "../common/Selector";
 
 export default function Digiterra() {
   const [categoryStatus, setCategoryStatus] = useState<StatusCategory>({
-    key: "all",
     name: "전체",
+    key: "all",
   });
-  const onStatusCategory = useCallback((status: CategoryType) => {
+
+  const [sortedStatus, setSortedItem] = useState<SortCategory>({
+    name: "추천 순",
+    key: "recommended",
+  });
+
+  const onStatusCategory = useCallback((status: SelectItemType) => {
     setCategoryStatus(status as StatusCategory);
+  }, []);
+
+  const onSortedCategory = useCallback((status: SelectItemType) => {
+    setSortedItem(status as SortCategory);
   }, []);
 
   return (
     <div>
       <PageHeader title="디지테라" />
+      <div className="px-4 py-2 h-14 w-full mt-[7.25rem] flex items-center justify-center space-x-2 border-b-[1px] border-b-slate-S200">
+        <Selector
+          list={statusList}
+          onSelected={onStatusCategory}
+          selectedItem={categoryStatus}
+          selectorBg="#2C3249"
+          selectedColor="#12162A"
+        />
+        <Selector
+          list={sortList}
+          onSelected={onSortedCategory}
+          selectedItem={sortedStatus}
+          selectorBg="#2C3249"
+          selectedColor="#12162A"
+        />
+      </div>
       <DigiterraList status={categoryStatus} />
     </div>
   );
@@ -24,6 +51,22 @@ export default function Digiterra() {
 
 export type StatusCategoryName = "전체" | "예정" | "진행중" | "마감";
 export type StatusCategoryKey = "all" | "upcoming" | "progress" | "close";
+
+export type SortCategoryName =
+  | "추천 순"
+  | "발행 수량 순"
+  | "마감 임박 순"
+  | "최신 순";
+export type SortCategoryKey =
+  | "recommended"
+  | "quantity"
+  | "deadline"
+  | "latest";
+
+type SortCategory = {
+  name: SortCategoryName;
+  key: SortCategoryKey;
+};
 
 type StatusCategory = {
   name: StatusCategoryName;
@@ -46,5 +89,24 @@ const statusList: StatusCategory[] = [
   {
     name: "마감",
     key: "close",
+  },
+];
+
+const sortList: SortCategory[] = [
+  {
+    name: "추천 순",
+    key: "recommended",
+  },
+  {
+    name: "발행 수량 순",
+    key: "quantity",
+  },
+  {
+    name: "마감 임박 순",
+    key: "deadline",
+  },
+  {
+    name: "최신 순",
+    key: "latest",
   },
 ];
